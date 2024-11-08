4ffdc4b6440e499e37a9de130a951b5042e98a18
offc_who <nhnhnhbgbg@gmail.com>
2024-11-08 07:52:50 +0300

    update

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,7 @@
 
-import localFont from "next/font/local";
 import "./scss/main.scss";
 import Header from "./components/header";
 import { montserrat } from "./components/font";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-
 
 export const metadata = {
   title: "Create Next App",
