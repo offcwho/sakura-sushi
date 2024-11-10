8ff8dad6e1bccb655542a6d1d5e4b3b6b3b96469
offc_who <nhnhnhbgbg@gmail.com>
2024-11-11 00:17:14 +0300

    update

--- app/components/reviews.js
+++ app/components/reviews.js
@@ -40,134 +40,144 @@ export default function Reviews(){
                         data-swiper-parallax="-23%"
                     ></div>
                     <SwiperSlide className='reviews__slide reviews__post'>
-                            <h3 className="reviews__post-title">Оставьте отзыв</h3>
-                            <p className="reviews__post-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quaerat ullam nulla nobis corporis, culpa tenetur ipsum exercitationem obcaecati quisquam!</p>
-                            <button className="reviews__button">
-                                <span className="reviews__button-text">Оставить отзыв</span>
-                            </button>
+                            <div className="reviews__container-slide">
+                                <h3 className="reviews__post-title" data-swiper-parallax="-100">Оставьте отзыв</h3>
+                                <p className="reviews__post-desc" data-swiper-parallax="-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quaerat ullam nulla nobis corporis, culpa tenetur ipsum exercitationem obcaecati quisquam!</p>
+                                <button className="reviews__button" data-swiper-parallax="-150">
+                                    <span className="reviews__button-text">Оставить отзыв</span>
+                                </button>
+                            </div>
                     </SwiperSlide>
                     <SwiperSlide className='reviews__slide'>
-                        <Image 
-                            src="/rating.svg"
-                            width={150}
-                            height={0}
-                            alt='Rating'
-                            className='reviews__rating'
-                            data-swiper-parallax="-150"
-                        />
-                        <div className="reviews__slide-header" data-swiper-parallax="-100">
-                            <Image
-                                src="/users/who_official.png"
-                                width={60}
-                                height={60}
-                                alt="User Image"
-                                className='reviews__userImage'
-                            />
-                            <h3 className="reviews__username">Username</h3>
-                        </div>
-                        <p className="reviews__review" data-swiper-parallax="-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere, numquam quos nostrum rem ab.</p>
-                        <div className="reviews__userPhoto-container">
+                        <div className="reviews__container-slide">
                             <Image 
-                                className='reviews__userPhoto'
-                                src="/user-photo/review_user-photo.jpg"
-                                width={400}
+                                src="/rating.svg"
+                                width={150}
                                 height={0}
-                                alt="Photo"
-                                data-swiper-parallax="-50"
+                                alt='Rating'
+                                className='reviews__rating'
+                                data-swiper-parallax="-150"
                             />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className='reviews__slide'>
-                        <Image 
-                            src="/rating.svg"
-                            width={150}
-                            height={0}
-                            alt='Rating'
-                            className='reviews__rating'
-                            data-swiper-parallax="-150"
-                        />
-                        <div className="reviews__slide-header" data-swiper-parallax="-100">
-                            <Image
-                                src="/users/who_official.png"
-                                width={60}
-                                height={60}
-                                alt="User Image"
-                                className='reviews__userImage'
-                            />
-                            <h3 className="reviews__username">Username</h3>
-                        </div>
-                        <p className="reviews__review" data-swiper-parallax="-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere, numquam quos nostrum rem ab.</p>
-                        <div className="reviews__userPhoto-container">
-                            <Image 
-                                className='reviews__userPhoto'
-                                src="/user-photo/review_user-photo.jpg"
-                                width={400}
-                                height={0}
-                                alt="Photo"
-                                data-swiper-parallax="-50"
-                            />
+                            <div className="reviews__slide-header" data-swiper-parallax="-100">
+                                <Image
+                                    src="/users/who_official.png"
+                                    width={60}
+                                    height={60}
+                                    alt="User Image"
+                                    className='reviews__userImage'
+                                />
+                                <h3 className="reviews__username">Username</h3>
+                            </div>
+                            <p className="reviews__review" data-swiper-parallax="-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere, numquam quos nostrum rem ab.</p>
+                            <div className="reviews__userPhoto-container">
+                                <Image 
+                                    className='reviews__userPhoto'
+                                    src="/user-photo/review_user-photo.jpg"
+                                    width={400}
+                                    height={0}
+                                    alt="Photo"
+                                    data-swiper-parallax="-50"
+                                />
+                            </div>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='reviews__slide'>
-                        <Image 
-                            src="/rating.svg"
-                            width={150}
-                            height={0}
-                            alt='Rating'
-                            className='reviews__rating'
-                            data-swiper-parallax="-150"
-                        />
-                        <div className="reviews__slide-header" data-swiper-parallax="-100">
-                            <Image
-                                src="/users/who_official.png"
-                                width={60}
-                                height={60}
-                                alt="User Image"
-                                className='reviews__userImage'
-                            />
-                            <h3 className="reviews__username">Username</h3>
-                        </div>
-                        <p className="reviews__review" data-swiper-parallax="-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere, numquam quos nostrum rem ab.</p>
-                        <div className="reviews__userPhoto-container">
+                        <div className="reviews__container-slide">
                             <Image 
-                                className='reviews__userPhoto'
-                                src="/user-photo/review_user-photo.jpg"
-                                width={400}
+                                src="/rating.svg"
+                                width={150}
                                 height={0}
-                                alt="Photo"
-                                data-swiper-parallax="-50"
+                                alt='Rating'
+                                className='reviews__rating'
+                                data-swiper-parallax="-150"
                             />
+                            <div className="reviews__slide-header" data-swiper-parallax="-100">
+                                <Image
+                                    src="/users/who_official.png"
+                                    width={60}
+                                    height={60}
+                                    alt="User Image"
+                                    className='reviews__userImage'
+                                />
+                                <h3 className="reviews__username">Username</h3>
+                            </div>
+                            <p className="reviews__review" data-swiper-parallax="-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere, numquam quos nostrum rem ab.</p>
+                            <div className="reviews__userPhoto-container">
+                                <Image 
+                                    className='reviews__userPhoto'
+                                    src="/user-photo/review_user-photo.jpg"
+                                    width={400}
+                                    height={0}
+                                    alt="Photo"
+                                    data-swiper-parallax="-50"
+                                />
+                            </div>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide className='reviews__slide'>
-                        <Image 
-                            src="/rating.svg"
-                            width={150}
-                            height={0}
-                            alt='Rating'
-                            className='reviews__rating'
-                            data-swiper-parallax="-150"
-                        />
-                        <div className="reviews__slide-header" data-swiper-parallax="-100">
-                            <Image
-                                src="/users/who_official.png"
-                                width={60}
-                                height={60}
-                                alt="User Image"
-                                className='reviews__userImage'
-                            />
-                            <h3 className="reviews__username">Username</h3>
-                        </div>
-                        <p className="reviews__review" data-swiper-parallax="-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere, numquam quos nostrum rem ab.</p>
-                        <div className="reviews__userPhoto-container">
+                        <div className="reviews__container-slide">
                             <Image 
-                                className='reviews__userPhoto'
-                                src="/user-photo/review_user-photo.jpg"
-                                width={400}
+                                src="/rating.svg"
+                                width={150}
                                 height={0}
-                                alt="Photo"
-                                data-swiper-parallax="-50"
+                                alt='Rating'
+                                className='reviews__rating'
+                                data-swiper-parallax="-150"
                             />
+                            <div className="reviews__slide-header" data-swiper-parallax="-100">
+                                <Image
+                                    src="/users/who_official.png"
+                                    width={60}
+                                    height={60}
+                                    alt="User Image"
+                                    className='reviews__userImage'
+                                />
+                                <h3 className="reviews__username">Username</h3>
+                            </div>
+                            <p className="reviews__review" data-swiper-parallax="-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere, numquam quos nostrum rem ab.</p>
+                            <div className="reviews__userPhoto-container">
+                                <Image 
+                                    className='reviews__userPhoto'
+                                    src="/user-photo/review_user-photo.jpg"
+                                    width={400}
+                                    height={0}
+                                    alt="Photo"
+                                    data-swiper-parallax="-50"
+                                />
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='reviews__slide'>
+                        <div className="reviews__container-slide">
+                            <Image 
+                                src="/rating.svg"
+                                width={150}
+                                height={0}
+                                alt='Rating'
+                                className='reviews__rating'
+                                data-swiper-parallax="-150"
+                            />
+                            <div className="reviews__slide-header" data-swiper-parallax="-100">
+                                <Image
+                                    src="/users/who_official.png"
+                                    width={60}
+                                    height={60}
+                                    alt="User Image"
+                                    className='reviews__userImage'
+                                />
+                                <h3 className="reviews__username">Username</h3>
+                            </div>
+                            <p className="reviews__review" data-swiper-parallax="-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere, numquam quos nostrum rem ab.</p>
+                            <div className="reviews__userPhoto-container">
+                                <Image 
+                                    className='reviews__userPhoto'
+                                    src="/user-photo/review_user-photo.jpg"
+                                    width={400}
+                                    height={0}
+                                    alt="Photo"
+                                    data-swiper-parallax="-50"
+                                />
+                            </div>
                         </div>
                     </SwiperSlide>
                 </Swiper>
