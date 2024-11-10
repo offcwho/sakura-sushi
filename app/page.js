233cc6c73917ccba3c7b0740baea436163b73028
offc_who <nhnhnhbgbg@gmail.com>
2024-11-10 09:16:05 +0300

    update

--- app/page.js
+++ app/page.js
@@ -1,11 +1,6 @@
-'use client'
-
 import Welcome from "./components/welcome";
 import About from "./components/about";
 import Menu from "./components/menu";
-import SmoothScroll from "smoothscroll-for-websites";
-
-SmoothScroll({ stepSize: 15 })
 
 export default function Home() {
   return (
