c0183e88d47bdbcdbba8adf4300bcb4bd4b60a5a
offc_who <nhnhnhbgbg@gmail.com>
2024-11-11 06:13:46 +0300

    update

---
+++ app/components/reservation.js
@@ -0,0 +1,9 @@
+export default function Reservation(){
+    return(
+        <section className="main__section reservation">
+            <div className="container reservation__container">
+                
+            </div>
+        </section>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import Welcome from "./components/welcome";
 import About from "./components/about";
 import Menu from "./components/menu";
 import Reviews from "./components/reviews";
+import Reservation from "./components/reservation";
 
 export default function Home() {
   return (
       <main className="application__main main">
         <Welcome/>
         <Menu/>
+        <Reservation/>
         <About/>
         <Reviews/>
       </main>
