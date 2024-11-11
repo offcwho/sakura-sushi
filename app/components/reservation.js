4311fc17abda12911ba9b23bbff005e44a7a21ef
offc_who <nhnhnhbgbg@gmail.com>
2024-11-11 08:59:56 +0300

    update

--- app/components/reservation.js
+++ app/components/reservation.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react"
 
 import 'intl-tel-input/build/css/intlTelInput.css';
 import {DatePicker} from "@nextui-org/date-picker";
+import {now, getLocalTimeZone} from "@internationalized/date";
 
 export default function Reservation(){
 
@@ -35,7 +36,14 @@ export default function Reservation(){
                         <input type="tel" className="reservation__input" id="phone" placeholder="(999) 999 99 99" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
                     </div>
                     <div className="reservation__container-input">
-                            <DatePicker as="div" label="Birth date" className="max-w-[284px]" />
+                        <DatePicker
+                            
+                            label="Event Date"
+                            variant="bordered"
+                            hideTimeZone
+                            showMonthAndYearPickers
+                            defaultValue={now(getLocalTimeZone())}
+                        />
                     </div>
                     <textarea name="abc" id="textarea" className="reservation__textarea" placeholder="Примечание"></textarea>
                     <button className="button reservation__button">Забронировать</button>
